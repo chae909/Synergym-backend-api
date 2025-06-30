33b26312ae1f7752b4a617f364b0a2bb85791832
Rosy <rosy.jihye.noh@gmail.com>
2025-06-30 15:49:40 +0900

    feat(cloudinary): create service, config, properties, controller & edit build

---
+++ src/main/java/org/synergym/backendapi/config/CloudinaryConfig.java
@@ -0,0 +1,27 @@
+package org.synergym.backendapi.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    private final CloudinaryProperties properties;
+
+    public CloudinaryConfig(CloudinaryProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        System.out.println("[CloudinaryConfig] Cloudinary bean registered.");
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", properties.getCloudName(),
+                "api_key", properties.getApiKey(),
+                "api_secret", properties.getApiSecret()
+        ));
+    }
+    
+}
