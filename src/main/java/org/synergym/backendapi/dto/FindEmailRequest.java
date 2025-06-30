e72335021574346f8afc7b1808b602f2e167165c
jeongyoon2004@naver.com <jeongyoon2004@naver.com>
2025-06-30 17:42:11 +0900

    feat: edit find ID & password functionality

--- src/main/java/org/synergym/backendapi/dto/FindEmailRequest.java
+++ src/main/java/org/synergym/backendapi/dto/FindEmailRequest.java
@@ -1,6 +1,8 @@
 package org.synergym.backendapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +12,10 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class FindEmailRequest {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotNull(message = "생년월일은 필수 입력 값입니다.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
