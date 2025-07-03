6193d24b32c59492ce900840884873d522df35f2
Rosy <rosy.jihye.noh@gmail.com>
2025-07-03 15:32:15 +0900

    edit thumbnail_url of exercise and import code

--- src/main/java/org/synergym/backendapi/util/ExerciseJsonImporter.java
+++ src/main/java/org/synergym/backendapi/util/ExerciseJsonImporter.java
@@ -1,18 +1,19 @@
 // package org.synergym.backendapi.util;
-
 // import com.fasterxml.jackson.core.type.TypeReference;
 // import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 // import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.core.io.ClassPathResource;
 // import org.springframework.stereotype.Component;
 // import org.synergym.backendapi.dto.ExerciseDTO;
 // import org.synergym.backendapi.service.ExerciseService;
-// import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 
 // import java.io.File;
 // import java.util.List;
 
 // @Component
-// public class ExerciseJsonImporter {
+// public class ExerciseJsonImporter implements CommandLineRunner {
 
 //     @Autowired
 //     private ExerciseService exerciseService;
@@ -20,12 +21,18 @@
 //     public void importFromJson(String jsonFilePath) throws Exception {
 //         ObjectMapper mapper = new ObjectMapper();
 //         mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+//         File file = new ClassPathResource(jsonFilePath).getFile();
 //         List<ExerciseDTO> exercises = mapper.readValue(
-//                 new File(jsonFilePath),
+//                 file,
 //                 new TypeReference<List<ExerciseDTO>>() {}
 //         );
 //         for (ExerciseDTO dto : exercises) {
 //             exerciseService.createExercise(dto);
 //         }
+//     }
+
+//     @Override
+//     public void run(String... args) throws Exception {
+//         importFromJson("seed/naver_exercise_cleaned_cloudinary.json");
 //     }
 // }
